f18440a7ae7874a178209f07bd21dcc63b9a5e53
Jack <jack@jack.com>
2023-04-04 12:33:45 +0500

    Add rest framework, djoser and related urls. Create order and client models. Into admin register models.

--- CRM_Project/main_app/models.py
+++ CRM_Project/main_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+User = get_user_model()
+
+
+class Order(models.Model):
+    defect = models.CharField(max_length=150)
+    created = models.DateTimeField(auto_now_add=True)
+    edited = models.DateTimeField(auto_now=True)
+
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    client = models.ForeignKey('Client', on_delete=models.PROTECT)
+
+
+class Client(models.Model):
+    first_name = models.CharField(max_length=21)
+    last_name = models.CharField(max_length=28)
+    phone = models.CharField(max_length=21)
+    address = models.CharField(max_length=42)
