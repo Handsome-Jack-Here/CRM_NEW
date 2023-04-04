dac32cd7afb71b1bbdbf2ed503bf3ca848305e9c
Jack <jack@jack.com>
2023-04-04 12:40:01 +0500

    Add individual orders numbers for users and override save method for correct work.

--- CRM_Project/main_app/models.py
+++ CRM_Project/main_app/models.py
@@ -5,12 +5,22 @@ User = get_user_model()
 
 
 class Order(models.Model):
+    order_id = models.IntegerField(null=True, blank=True)
     defect = models.CharField(max_length=150)
     created = models.DateTimeField(auto_now_add=True)
     edited = models.DateTimeField(auto_now=True)
 
     user = models.ForeignKey(User, on_delete=models.PROTECT)
     client = models.ForeignKey('Client', on_delete=models.PROTECT)
+
+    def save(self, *args, **kwargs):
+        user = self.user
+        orders_count = user.orders.count()
+        self.order_id = orders_count + 1
+        return super(Order, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return f'{self.pk} {self.defect} {self.client}'
 
 
 class Client(models.Model):
