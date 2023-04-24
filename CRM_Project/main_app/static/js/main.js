a7987a1b483b57215969756ce6a1ee264063edff
Jack <jack@jack.com>
2023-04-24 22:02:55 +0500

    Complete new order creation for clients fields and some order fields.

--- CRM_Project/main_app/static/js/main.js
+++ CRM_Project/main_app/static/js/main.js
@@ -22,14 +22,13 @@ $(document).ready(function () {
 
 
     async function newOrderSave() {
-        $('#new_order_form').attr('hidden', true);
+        $('#new_order').attr('hidden', true);
 
-        let order = {
+        let client_fields = {
             'first_name': $('#new_order_form #first_name').val(),
             'last_name': $('#new_order_form #last_name').val(),
             'phone': $('#new_order_form #phone_number').val(),
         }
-
         let options = {
 
             method: 'POST',
@@ -37,23 +36,38 @@ $(document).ready(function () {
                 "Content-Type": "application/json",
                 'X-CSRFToken': csrftoken,
             },
-            body: JSON.stringify(order)
+            body: JSON.stringify(client_fields)
         }
 
         const response = await fetch(`/api/v1/clients/`, options)
         let data = await response.json()
-        let client_id = JSON.stringify(data.id)
+        let client_id = parseInt(JSON.stringify(data.id))
+
+        let order_fields = {
+            'defect': $('#new_order_form #defect').val(),
+            'client': client_id
+        }
+        options = {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json",
+                'X-CSRFToken': csrftoken,
+            },
+            body: JSON.stringify(order_fields)
+        }
+
+        await fetch(`/api/v1/orders/`, options)
 
 
         if (response.status === 201) {
-            $('#client_form').each(function () {
+            $('#new_order_form').each(function () {
                 this.reset()
                 getOrderList().then($('#order_list').fadeIn(200))
             })
 
         } else {
             alert(response.status)
-            $('#new_order_form').removeAttr('hidden');
+            $('#new_order').removeAttr('hidden');
         }
 
 
@@ -65,6 +79,8 @@ $(document).ready(function () {
 
         let order = {
             'defect': $('#defect').val(),
+            'diagnostic_result': $('#diagnostic_result').val(),
+            'required_works': $('#required_works').val(),
             'first_name': $('#first_name').val(),
             'last_name': $('#last_name').val(),
             'phone': $('#phone_number').val(),
@@ -102,7 +118,7 @@ $(document).ready(function () {
 
     async function getOrderDetail(order_id) {
 
-        let check_summ = []
+
 
         $('#order_list').hide();
         $('#order_detail').removeAttr('hidden');
@@ -113,12 +129,14 @@ $(document).ready(function () {
         $('#order_id').text('Order# ' + order_id)
 
         $('#defect').val(order.defect);
+        $('#diagnostic_result').val(order.diagnostic_result);
+        $('#required_works').val(order.required_works);
+
         $('#first_name').val(client.first_name);
         $('#last_name').val(client.last_name);
         $('#phone_number').val(client.phone);
 
-        check_summ += JSON.stringify(order) + JSON.stringify(client)
-        // alert(check_summ)
+
 
         $('#save').off().click(function (e) {
             e.preventDefault();
@@ -142,14 +160,14 @@ $(document).ready(function () {
     });
 
 
-    $('#new_order').on('click', function () {
+    $('#new_order_button').on('click', function () {
         $('#order_list').hide();
-        $('#new_order_form').removeAttr('hidden');
+        $('#new_order').removeAttr('hidden');
     })
 
 // validate forms
     $('#create_order').on('click', function () {
-        $('#client_form').validate({
+        $('#new_order_form').validate({
 
             rules: {
                 first_name: {
@@ -163,9 +181,13 @@ $(document).ready(function () {
                 phone_number: {
                     required: true,
                     minlength: 10,
-
+                },
+                defect: {
+                    required: true,
+                    minlength: 7,
                 }
             },
+
             messages: {
                 first_name: {
                     required: 'This field is required',
@@ -179,10 +201,14 @@ $(document).ready(function () {
                     required: 'This field is required',
                     minlength: 'This field mest be at least 10 characters',
                 },
+                defect: {
+                    required: 'This field is required',
+                    minlength: 'This field mest be at least 7 characters'
+                },
             }
         })
 
-        if ($('#client_form').valid()) {
+        if ($('#new_order_form').valid()) {
             newOrderSave()
         }
 
